0fe6f1fe2fb67fa302d511e1391e3d3795969eec
pegel <peter.gelsbo@gmail.com>
2014-06-19 23:50:02 +0200

    License added

--- rtemp_finish/src/View/View.java
+++ rtemp_finish/src/View/View.java
@@ -1,3 +1,19 @@
+/*
+    Copyright 2014 Peter Gelsbo and Andreas Nordmand Andersen
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	    http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
 package View;
 
 import org.eclipse.swt.SWT;
@@ -5,7 +21,6 @@ import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
@@ -23,7 +38,6 @@ import Model.TopologyTypes;
 import Model.Aegean.Aegean;
 import Model.Aegean.Include;
 import Model.IPCores.IPCore;
-import Model.IPCores.IPCores;
 import Static.OSFinder;
 import Static.Settings;
 
--- rtemp_finish/src/View/ViewToolbarPlatform.java
+++ rtemp_finish/src/View/ViewToolbarPlatform.java
@@ -1,14 +1,26 @@
+/*
+    Copyright 2014 Peter Gelsbo and Andreas Nordmand Andersen
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	    http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
 package View;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.ToolBar;
